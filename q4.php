5474ac7ee32a8c00a217f37934d076e568243c0b
M.R Sandaruwan <151626521+Raviduofficial@users.noreply.github.com>
2024-09-29 16:23:46 -1200

    Add files via upload

---
+++ q4.php
@@ -0,0 +1,96 @@
+
+
+
+
+
+
+<html>
+<body>
+
+<?php
+	/*function addFive(&$num){
+		$num += 5;
+	}
+	$orignum =10;
+	addFive($orignum);
+	echo "Original Value is $orignum <br/>";
+
+
+
+	function addFive($num){
+		$num += 5;
+		echo " in function Value is $num <br/>";
+	}
+	$orignum = 10;
+	addFive($orignum);
+	echo "Original Value is $orignum <br/>";
+	
+	
+	
+	function printArg(){
+		$numargs = func_num_args();
+		echo "Number of argument: $numargs \n";
+	}
+	printArg(1,2,3);
+	
+	
+	
+	function showArg(){
+		$numargs = func_num_args();
+		echo "Number of argument: $numargs <br/>";
+		if($numargs >= 2){
+			echo "Second Argument is ".func_get_arg(1)."\n";
+		}
+		
+	}
+	showArg(1,5,3);
+	
+	
+	
+	 function showArray(){
+		$numargs= func_num_args();
+		echo "Number of arguments:$numargs\n";
+	if ($numargs >= 2) {
+		echo "Second argumentis: " . func_get_arg(1) . "\n";
+	}
+		$arg_list = func_get_args();
+	for ($i = 0; $i < $numargs;$i++) {
+		echo"Argument$i is: " . $arg_list[$i] . "\n";
+	}
+		}
+	showArray(1, 5, 3);
+	showArray(1, 5, 3);
+	
+	
+	 echo(date("d"). "<br/>");
+	 echo(date("D"). "<br/>");
+	 echo(date("m"). "<br/>");
+	 echo(date("M") . "<br/>");
+	 echo(date("y") . "<br/>");
+	 echo(date("Y"). "<br/>");
+	
+	echo(date("z")."<br/>");
+	echo(date("H").":".date("i").":".date("s")."".date("a")."<br>");
+	
+	
+	function fact($n){
+		$val = $n * ($n-1)*($n-2)*($n-3)*($n-4);
+		echo"$val";
+		$val=1;
+		for($i = $n;$i>=1;$i--){
+			$val *= $i;
+		}
+		echo"$val";
+	}
+	fact(5);
+	*/
+	
+	function isPalindrome(&$str){
+		$left = 0
+		$right = strlen($str)
+	}
+	
+?>
+
+</body>
+</html>
